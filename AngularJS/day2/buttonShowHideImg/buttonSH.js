c3e09459ffce1240494970931c3090d69a1f8524
zhangloo333 <zhangloo33@gmail.com>
2015-11-11 14:29:12 -0800

    add delet row which you want

--- AngularJS/day2/buttonShowHideImg/buttonSH.js
+++ AngularJS/day2/buttonShowHideImg/buttonSH.js
@@ -11,14 +11,29 @@ imgApp.controller("imgcontr", function ($scope) {
     };
     $scope.items=["monitor","karbard","mouse"];
 
-    $scope.order=[{'name':'monitor','price':'$50'},
-                  {'name':'tash','price':'$60'},
-        {'name':'shen','price':'$59'},
-        {'name':'asdasd','price':'$90'},
-        {'name':'asdd','price':'$34'},
-        {'name':'teer','price':'$24'}];
+
 
     $scope.creatT= function () {
+        $scope.order=[{'name':'monitor','price':'$50'},
+            {'name':'tash','price':'$60'},
+            {'name':'shen','price':'$59'},
+            {'name':'asdasd','price':'$90'},
+            {'name':'asdd','price':'$34'},
+            {'name':'teer','price':'$24'}];
 
     }
-})
+
+    $scope.deleteF= function () {
+        $scope.order.pop();
+    }
+
+    $scope.deleteL=function(){
+        $scope.order.shift();
+    }
+
+    $scope.deleteR= function (i) {
+        $scope.order.splice(i,1);
+
+    }// for delete whatever row you want
+
+});
