a4d789abfed6957e015d57f22696d28f7ce6692e
zhangloo333 <zhangloo33@gmail.com>
2015-11-12 22:00:54 -0800

    day3 basic practics

---
+++ AngularJS/day2/switchAndIF/switchIf.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lei on 2015/11/11.
+ */
+var myapp = angular.module("samepleApp",[]);
+
+myapp.controller("ifSwitch", function ($scope) {
+    $scope.info=true;
+    $scope.showIf= function () {
+        $scope.info=!$scope.info;
+    };
+    $scope.showSwitch=function(){
+        $scope.country="china";
+    };
+});
